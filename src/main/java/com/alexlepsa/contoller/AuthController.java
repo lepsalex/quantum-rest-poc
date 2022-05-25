166f4116545abf74db59725fd3abfe4531243d82
Alex <lepsalex@gmail.com>
2022-05-25 12:48:11 -0400

    auth update

--- src/main/java/com/alexlepsa/contoller/AuthController.java
+++ src/main/java/com/alexlepsa/contoller/AuthController.java
@@ -1,17 +1,31 @@
 package com.alexlepsa.contoller;
 
+import com.alexlepsa.contoller.model.AuthResponse;
+import io.micronaut.http.HttpHeaders;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Header;
-import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.*;
 import io.micronaut.validation.Validated;
+import lombok.val;
+
+import java.util.List;
 
 import static com.alexlepsa.contoller.common.AuthUtils.AUTH_HEADER;
 import static com.alexlepsa.contoller.common.AuthUtils.isAccountIdAuthed;
+import static com.alexlepsa.contoller.model.AuthResponse.authResponseOk;
+import static com.alexlepsa.contoller.model.AuthResponse.authResponseUnauthorized;
 
 @Validated
 @Controller("/auth")
 public class AuthController {
+
+    @Post
+    public HttpResponse<AuthResponse> auth(@Body String authToken) {
+        if (isAccountIdAuthed(authToken)) {
+            return HttpResponse.ok(authResponseOk(authToken));
+        }
+
+        return HttpResponse.ok(authResponseUnauthorized());
+    }
 
     @Post("/room/{roomId}")
     public HttpResponse<String> authRoom(@Header(AUTH_HEADER) String authToken, String roomId) {
---
+++ src/main/java/com/alexlepsa/contoller/model/AuthResponse.java
@@ -0,0 +1,37 @@
+package com.alexlepsa.contoller.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.val;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+public class AuthResponse {
+    private Integer resultCode;
+    private AuthCookie authCookie;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+    public static class AuthCookie {
+        private String token;
+    }
+
+    public static AuthResponse authResponseOk(String token) {
+        val authCookie = new AuthCookie(token);
+
+        return new AuthResponse(1, authCookie);
+    }
+
+    public static AuthResponse authResponseUnauthorized() {
+        return new AuthResponse(2, null);
+    }
+}
