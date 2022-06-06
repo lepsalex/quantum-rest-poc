5c413830dc342e18304c2103b09627da36d166fd
Alex <lepsalex@gmail.com>
2022-06-06 12:36:03 -0400

    rest to websocket message bridge

--- src/main/java/com/alexlepsa/contoller/RoomSessionController.java
+++ src/main/java/com/alexlepsa/contoller/RoomSessionController.java
@@ -3,17 +3,24 @@ package com.alexlepsa.contoller;
 import com.alexlepsa.repository.RoomSessionRepository;
 import com.alexlepsa.repository.model.RoomSession;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.validation.Validated;
 import jakarta.inject.Inject;
+import lombok.val;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Validated
 @Controller("/room-session")
 public class RoomSessionController {
+
+    @Inject
+    private WebsocketController websocketController;
 
     @Inject
     private RoomSessionRepository roomSessionRepository;
@@ -23,5 +30,12 @@ public class RoomSessionController {
         List<RoomSession> roomSessions = new ArrayList<>();
         roomSessionRepository.findAll().iterator().forEachRemaining(roomSessions::add);
         return HttpResponse.ok(roomSessions);
+    }
+
+    @Post()
+    public HttpResponse<String> sendCommandToSession(@Body String command) {
+        val emitResult = websocketController.getCommands().tryEmitNext(command);
+
+        return emitResult.isSuccess() ? HttpResponse.ok() : HttpResponse.serverError();
     }
 }
--- src/main/java/com/alexlepsa/contoller/WebsocketController.java
+++ src/main/java/com/alexlepsa/contoller/WebsocketController.java
@@ -9,11 +9,30 @@ import io.micronaut.websocket.annotation.OnMessage;
 import io.micronaut.websocket.annotation.OnOpen;
 import io.micronaut.websocket.annotation.ServerWebSocket;
 import jakarta.inject.Inject;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Disposable;
+import reactor.core.publisher.Sinks;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @ServerWebSocket("/ws/commands")
 public class WebsocketController {
+
+    @Getter
+    private final Sinks.Many<String> commands = Sinks.many().multicast().onBackpressureBuffer();
+
+    @Getter
+    private Disposable commandsDisposable;
+
+    @PostConstruct
+    public void init() {
+        commandsDisposable = commands
+                .asFlux()
+                .doOnNext(command -> broadcaster.broadcastSync(command))
+                .subscribe();
+    }
 
     @Inject
     private RoomSessionRepository roomSessionRepository;
